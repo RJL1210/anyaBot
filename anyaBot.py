d34add8807c9ef5b5842e8d6a1686394da166ee1
Ryan Li <rjl12101@gmail.com>
2023-10-12 22:30:47 -0700

    Changed fundamentally how the bot responds. Uses commands instead of detecting every chat. Added lots of functionality to the weather command specifically.

--- anyaBot.py
+++ anyaBot.py
@@ -1,12 +1,16 @@
 import discord
+import requests
 from discord.ext import commands
+import weather
 import responses
 import json
 
 #get api
 tokens = open('config.json')
 json_contents = json.load(tokens)
-api_key = json_contents['botToken']
+discord_key = json_contents['botToken']
+
+
 
 
 
@@ -20,16 +24,80 @@ async def send_message(message, user_message):
         print(e)
 
 def run_discord_bot():
-    intents = discord.Intents.default()
+    intents = discord.Intents.all()
     intents.message_content = True
-    client = commands.Bot(command_prefix='.', intents = discord.Intents.all())
+    #bot will recognize commands with the .
+    client = commands.Bot(command_prefix=".", intents = intents)
+
+    @client.command()
+    async def weatherAt(ctx, city):
+        temp = weather.get_weather(city)
+        if (temp == "No City Found"):
+            await ctx.send(temp)
+        #need different images for different weather
+        match temp[1]:
+            case 'Clear':
+                file = discord.File("/Users/rjl12/Downloads/WeatherIcons/sun.png", filename = "image.png")
+                embed = discord.Embed(title = "Weather: Sunny", description = "Degrees: " + temp[0])
+                embed.set_image(url = "attachment://image.png")
+                await ctx.send(file = file, embed = embed)
+            case 'overcast clouds':
+                file = discord.File("/Users/rjl12/Downloads/WeatherIcons/cloudy.png", filename = "image.png")
+                embed = discord.Embed(title = "Weather: Cloudy", description = "Degrees: " + temp[0])
+                embed.set_image(url = "attachment://image.png")
+                await ctx.send(file = file, embed = embed)
+            case 'few clouds':
+                file = discord.File("/Users/rjl12/Downloads/WeatherIcons/partlycloudy.png", filename = "image.png")
+                embed = discord.Embed(title = "Weather: Partly Cloudy", description = "Degrees: " + temp[0])
+                embed.set_image(url = "attachment://image.png")
+                await ctx.send(file = file, embed = embed)
+            case 'Rain':
+                file = discord.File("/Users/rjl12/Downloads/WeatherIcons/rainy.png", filename = "image.png")
+                embed = discord.Embed(title = "Weather: Rainy", description = "Degrees: " + temp[0])
+                embed.set_image(url = "attachment://image.png")
+                await ctx.send(file = file, embed = embed)
+            #case 'Haze':
+            
+            #case 'Mist':
+
+        #await ctx.send(embed = embed)
+
+    @client.command()
+    async def time(ctx, city):
+        date_and_time = weather.get_time(city)
+        time_api = date_and_time.split(' ')
+        embed = discord.Embed(title = "Time: " + time_api[2], description = "Idk ill put a picture here hopefully")
+        await ctx.send(embed = embed)
+
+    @client.command()
+    async def shutdown(ctx):
+        await ctx.send("Shutting down")
+        await client.close()
+
+    @client.command()
+    async def hello(ctx):
+        await ctx.send("Waku Waku")
+
+    @client.command()
+    async def olof(ctx):
+        embed = discord.Embed(color = 0x00ff00)
+        file = discord.File("/Users/rjl12/Downloads/bocchiolof.jpg", filename = "image.png")
+        embed.set_image(url = "attachment://image.png")
+        await ctx.send(file = file, embed = embed)
+
+    @client.command()
+    async def sadge(ctx):
+        embed = discord.Embed()
+        embed.set_image(url = "https://media.tenor.com/0qj0aqZ0nucAAAAC/anya-spy-x-family-anime-anya-crying.gif")
+        await ctx.channel.send(embed = embed)
+        
 
 
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
-
-
+    
+    '''
     @client.event
     async def on_message(message):
 
@@ -40,17 +108,11 @@ def run_discord_bot():
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-        
-        '''does not work idk why want to implement a shutdown
-        
-        if user_message is ".shutdown":
-            async def shutdown(ctx):
-                await ctx.send("Shutting down")
-                await client.close()
-        '''
 
         print(f'{username} said: "{user_message}" ({channel})')
         
         await send_message(message, user_message)
+    '''
+   
     
-    client.run(api_key)
+    client.run(discord_key)
--- weather.py
+++ weather.py
@@ -12,21 +12,27 @@ def temp_conversion(kelvin):
     return fahrenheit
 
 
-#print(response)
 
 
 
-def get_weather(city) -> str:
+
+def get_weather(city):
     weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
     response = requests.get(weather_url).json()
-
+    print(response)
     if response['cod'] == '404':
         return "No City Found"
     
+    sub_weather = response['weather'][0]['description']
+    main_weather = response['weather'][0]['main']
+    #so many different subtypes of cloudy weather, the description is more important in this case
+    if (main_weather == 'Clouds'):
+        main_weather = sub_weather
+    print(main_weather)
     temp_kelvin = response['main']['temp']
     temp_fahren = "{:.2f}".format(temp_conversion(temp_kelvin))
 
-    return 'Degrees: ' + temp_fahren
+    return (temp_fahren, main_weather)
 
 def get_time(city) -> str:
     weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
